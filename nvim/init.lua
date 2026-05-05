8484fee23c096410e12ca58cf9690ee6d2702cfa
Leonardo Salazar <31389063+SCLeoo@users.noreply.github.com>
2026-05-05 15:50:24 -0600

    Refactor Neovim configuration for Trinity integration

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,17 +8,17 @@
 vim.g.mapleader = " "
 
 -- 2. Syntax Shortcuts (Local variables for cleaner code)
-local opt = vim.opt 
+local opt = vim.opt
 
 -- 3. Line Numbers (The perfect combo for fast movement)
-opt.number = true          -- Show the absolute number of the current line
-opt.relativenumber = true  -- Show distance from the current line (e.g., "5 lines down")
-                           -- This is key for jumping: if you see a 10 above, type "10k".
+opt.number = true         -- Show the absolute number of the current line
+opt.relativenumber = true -- Show distance from the current line (e.g., "5 lines down")
+-- This is key for jumping: if you see a 10 above, type "10k".
 
 -- 4. Indentation (Following 2-space preference)
-opt.tabstop = 2      -- Number of visual spaces a Tab occupies
-opt.shiftwidth = 2   -- Number of spaces inserted for indentation ('>' or '<<')
-opt.expandtab = true -- Automatically convert Tabs into spaces
+opt.tabstop = 2       -- Number of visual spaces a Tab occupies
+opt.shiftwidth = 2    -- Number of spaces inserted for indentation ('>' or '<<')
+opt.expandtab = true  -- Automatically convert Tabs into spaces
 opt.autoindent = true -- Copy indentation from the previous line on Enter
 
 -- 5. Interface & Experience
@@ -31,18 +31,13 @@ opt.splitbelow = true    -- Horizontal splits open below
 
 -- 6. Clipboard (System synchronization)
 -- This allows copy-pasting between Neovim and Windows/Mac/Linux apps.
-opt.clipboard = "unnamedplus"
-vim.g.clipboard = {
-  name = 'osc52',
-  copy = { ['+'] = require('vim.ui.clipboard.osc52').copy('+') },
-  paste = { ['+'] = require('vim.ui.clipboard.osc52').paste('+') },
-}
+opt.clipboard:append("unnamedplus")
 
 -- 7. "Black Hole" Deletion
 -- These mappings ensure that deleting text doesn't overwrite your clipboard.
 -- Normal and Visual mode: 'd' and 'x' send text to the '_' (Black Hole) register.
-vim.keymap.set({"n", "v"}, "d", '"_d', { desc = "Delete without yanking" })
-vim.keymap.set({"n", "v"}, "x", '"_x', { desc = "Delete character without yanking" })
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete character without yanking" })
 
 -- Note: To 'Cut' (Delete and copy), you can still Yank (y) then Delete (d).
 -- This keeps your clipboard clean from accidental deletions.
@@ -52,15 +47,14 @@ vim.keymap.set({"n", "v"}, "x", '"_x', { desc = "Delete character without yankin
 -- ========================================================================== --
 
 -- 1. Detect Operating System
--- Use vim.uv if available (v0.10+), otherwise fallback to vim.loop (older versions)
-local uv = vim.uv or vim.loop
-local sysname = uv.os_uname().sysname
+-- Uses 'vim.uv' (modern Neovim API) to identify the host system for logic/themes.
+local sysname = vim.uv.os_uname().sysname
 
 -- 2. The Bootstrapper (Auto-installer)
 -- This block checks if 'lazy.nvim' is installed. If not, it clones it from GitHub.
 -- This makes your configuration 100% portable to any new machine.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
@@ -77,14 +71,14 @@ vim.opt.rtp:prepend(lazypath)
 --                               PLUGINS SETUP                                --
 -- ========================================================================== --
 
--- 1. Plugin Configuration
+-- 3. Plugin Configuration
 -- This is where the "superpowers" of Neovim are defined.
 require("lazy").setup({
-  -- a. Icons (Required for a professional look in nvim-tree)
+  -- Icons (Required for a professional look in nvim-tree)
   { "nvim-tree/nvim-web-devicons" },
 
-  -- b. Nvim-Tree: The File Explorer
-  { 
+  -- Nvim-Tree: The File Explorer
+  {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
@@ -97,69 +91,182 @@ require("lazy").setup({
           number = true,
         },
         filters = {
-          dotfiles = false,      -- Show hidden files (like .env or .gitignore)
+          dotfiles = false, -- Show hidden files (like .env or .gitignore)
         },
       })
     end
   },
-    
-  -- c. Themes
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "ellisonleao/gruvbox.nvim", priority = 1000 },
-  { "Mofiqul/dracula.nvim", priority = 1000 },
 
--- [TRINITY SECTION] 
--- 
---   -- 1. MASON: The Package Manager for your Tools
---   -- This installs the actual binaries for Java, C#, etc.
---   -- { "williamboman/mason.nvim", config = true },
---   -- { "williamboman/mason-lspconfig.nvim", config = true },
--- 
---   -- 2. LSPCONFIG: The Bridge
---   -- This tells Neovim how to communicate with the installed servers.
---   -- { "neovim/nvim-lspconfig" },
--- 
---   -- 3. NVIM-CMP: The Autocompletion Engine
---   -- The interactive menu that pops up as you type code.
---   -- {
---   --   "hrsh7th/nvim-cmp",
---   --   dependencies = {
---   --     "hrsh7th/cmp-nvim-lsp", -- Links CMP to the LSP data
---   --     "L3MON4D3/LuaSnip",     -- Snippet engine for boilerplate code
---   --     "saadparwaiz1/cmp_luasnip",
---   --   },
---   --   config = function()
---   --     local cmp = require('cmp')
---   --     cmp.setup({
---   --       snippet = {
---   --         expand = function(args) require('luasnip').lsp_expand(args.body) end,
---   --       },
---   --       mapping = cmp.mapping.preset.insert({
---   --         ['<C-Space>'] = cmp.mapping.complete(), -- Manually trigger completion
---   --         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm with Enter
---   --         ['<Tab>'] = cmp.mapping.select_next_item(),
---   --         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
---   --       }),
---   --       sources = cmp.config.sources({
---   --         { name = 'nvim_lsp' },
---   --         { name = 'luasnip' },
---   --       }, {
---   --         { name = 'buffer' }, -- Suggest words existing in the current file
---   --       })
---   --     })
---   --   end
---   -- },
+  -- Themes
+  { "catppuccin/nvim",            name = "catppuccin", priority = 1000 },
+  { "ellisonleao/gruvbox.nvim",   priority = 1000 },
+  { "Mofiqul/dracula.nvim",       priority = 1000 },
+
+  -- ==========================================================================
+  --                             [TRINITY SECTION]
+  --                          Stage 1: Basic LSP only
+  -- ==========================================================================
+
+  {
+    "mason-org/mason.nvim",
+    dependencies = {
+      "mason-org/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "stevearc/conform.nvim",
+      "mfussenegger/nvim-lint",
+    },
+    config = function()
+      -- 1. Start Mason (tool installer)
+      require("mason").setup()
+
+      local mason_lspconfig = require("mason-lspconfig")
+
+      -- 3. Configure the Lua language server
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" }, -- stops "undefined global vim" errors
+              },
+            },
+          },
+        },
+
+        -- Config files (JSON, YAML)
+        jsonls = {},
+        yamlls = {},
+
+        -- Web
+        ts_ls = {}, -- TypeScript/JavaScript LSP (typescript-language-server)
+        cssls = {}, -- CSS
+
+        -- .NET / C#
+        omnisharp = {}, -- OmniSharp (C#/VB)
+
+        -- Java
+        jdtls = {}, -- Eclipse JDT Language Server
+      }
+
+      local ensure = {}
+      for name, _ in pairs(servers) do
+        table.insert(ensure, name)
+      end
+
+      mason_lspconfig.setup({
+        ensure_installed = ensure,
+        automatic_enable = true,
+      })
+
+      for name, cfg in pairs(servers) do
+        cfg.capabilities = capabilities
+        vim.lsp.config(name, cfg)
+      end
+
+      local conform = require("conform")
+
+      conform.setup({
+        -- Map filetypes to formatters.
+        -- Start small; expand later.
+        formatters_by_ft = {
+          -- Neovim config
+          lua = { "stylua" },
+          -- Web / config files
+          typescript = { "prettier" },
+          typescriptreact = { "prettier" },
+          javascript = { "prettier" },
+          javascriptreact = { "prettier" },
+          css = { "prettier" },
+          scss = { "prettier" },
+          html = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+          -- .NET
+          cs = { "csharpier" },
+          -- Java
+          java = { "google-java-format" },
+        },
+      })
+
+      vim.keymap.set({ "n", "v" }, "<leader>f", function()
+        conform.format({ lsp_fallback = true, timeout_ms = 2000 })
+      end, { desc = "Format file" })
+
+      -- Stage 2: Completion UI (nvim-cmp) - minimal setup
+      local cmp = require("cmp")
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "path" },
+          { name = "buffer" },
+        }),
+      })
+
+      -- ----------------------------
+      -- 7) Linting (nvim-lint)
+      -- ----------------------------
+      local lint = require("lint")
+
+      -- Start small: TypeScript/JS only. Add more later.
+      lint.linters_by_ft = {
+        typescript = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        javascript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+
+        -- Optional later (uncomment if you install these linters):
+        -- css = { "stylelint" },
+        -- scss = { "stylelint" },
+        -- json = { "jsonlint" },
+        -- yaml = { "yamllint" },
+      }
+
+      -- Windows nuance: sometimes Mason/npm provides eslint_d as eslint_d.cmd
+      -- If eslint_d doesn't run, this avoids "not executable" issues on Windows.
+      if vim.fn.executable("eslint_d.cmd") == 1 then
+        lint.linters.eslint_d.cmd = "eslint_d.cmd"
+      end
+
+      local lint_group = vim.api.nvim_create_augroup("trinity_lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+        group = lint_group,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      -- Manual trigger (useful for debugging)
+      vim.keymap.set("n", "<leader>ll", function()
+        lint.try_lint()
+      end, { desc = "Lint: run for current file" })
+    end,
+  },
+
 })
 
 -- ========================================================================== --
 --                               UI & THEMES                                  --
 -- ========================================================================== --
 
--- 1. OS-Based Color Logic
+-- 4. OS-Based Color Logic
 -- Automatically apply a different theme depending on the environment.
 if sysname == "Linux" then
   vim.cmd("colorscheme gruvbox")
-elseif sysname == "Windows_NT" then 
+elseif sysname == "Windows_NT" then
   vim.cmd("colorscheme dracula")
 else
   -- Catppuccin with transparency setup for macOS/Other
@@ -176,6 +283,17 @@ end
 vim.keymap.set("n", "<leader>e", function()
   require("nvim-tree.api").tree.toggle({ focus = true })
 end, { desc = "Focus File Explorer" })
+
+
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {
+  desc = "Diagnostics: open float",
+})
+vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {
+  desc = "Diagnostics: next",
+})
+vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {
+  desc = "Diagnostics: previous",
+})
 
 -- Auto-open on Startup
 -- Tells Neovim to open the tree as soon as the editor starts.
